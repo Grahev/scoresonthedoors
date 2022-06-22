a68c5898de7a1e82edb8c2911449e6cb05e8e663
Grahev <mumin20@gmail.com>
2022-06-23 00:15:03 +0100

    fix settings

--- app_core/settings.py
+++ app_core/settings.py
@@ -166,6 +166,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
