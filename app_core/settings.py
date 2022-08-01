d4a3c9d831882c3bec1ce217a90c089fb9edd125
Grahev <mumin20@gmail.com>
2022-08-01 21:52:29 +0100

    add domain to settings

--- app_core/settings.py
+++ app_core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vast-wildwood-19757.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vast-wildwood-19757.herokuapp.com', '127.0.0.1','scoresonthedoors.uk']
 
 
 # Application definition
