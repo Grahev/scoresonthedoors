5bebdcf4d34011276d6a2828e3a76d4bce1111a5
Grahev <mumin20@gmail.com>
2022-10-09 23:10:54 +0100

    test allowed all hosts

--- app_core/settings.py
+++ app_core/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vast-wildwood-19757.herokuapp.com', '127.0.0.1','scoresonthedoors.uk', 'web-production-7b21.up.railway.app']
+# ALLOWED_HOSTS = ['vast-wildwood-19757.herokuapp.com', '127.0.0.1','scoresonthedoors.uk', 'web-production-7b21.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
